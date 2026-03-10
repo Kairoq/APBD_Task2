10d5be89d1fdae9b11e33121220b0358faebbc3a
Kairoq <maksik771@wp.pl>
2026-03-10 22:55:35 +0100

    Removing the APBD2 folder v.3

---
+++ APBD2/Program.cs
@@ -0,0 +1,26 @@
+﻿var list = new double[2];
+for (var i = 0; i <= 1; i++)
+{
+    list[i] = Convert.ToDouble(Console.ReadLine());
+}
+
+Console.WriteLine(Addition(list[0], list[1]));
+Console.WriteLine(Subtraction(list[0], list[1]));
+Console.WriteLine(Average(list[0], list[1]));
+
+
+static double Addition(double b1, double b2)
+    {
+        return b1 + b2;
+    }
+
+static double Subtraction(double b1, double b2)
+{
+    return b1 - b2;
+}
+
+double Average(double b1, double b2)
+{
+    return b1 + b2 / list.Length;
+}
+    
