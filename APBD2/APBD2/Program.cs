d42613b62a1c3e78abb1f2fea960f152503f5f66
Kairoq <galmaksymilian@gmail.com>
2026-03-10 16:41:53 +0100

    Added simple read and addition

---
+++ APBD2/APBD2/Program.cs
@@ -0,0 +1,7 @@
+﻿
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(a);
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(b);
+double c = a+b;
+Console.WriteLine(c);
